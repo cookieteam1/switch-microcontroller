6af8fb1a64421c9e866acf04cabd151a521c13cb
daniel <daniel.zieger@fau.de>
2022-01-28 18:18:56 +0100

    typing

--- template.py
+++ template.py
@@ -9,12 +9,12 @@ SERIAL_DEFAULT = 'COM1' if sys.platform == 'win32' else '/dev/ttyUSB0'
 
 def main() -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument('--serial', default=SERIAL_DEFAULT)
-    parser.add_argument('--baud', default=9600)#opencv frame height
-    parser.add_argument('--camera', default=0)
-    parser.add_argument('--camAPI', default=0)#opencv videocapture api
-    parser.add_argument('--frameW', default=640)#opencv frame width
-    parser.add_argument('--frameH', default=480)#opencv frame height
+    parser.add_argument('--serial', default=SERIAL_DEFAULT, type=str)
+    parser.add_argument('--baud', default=9600, type=int)#opencv frame height
+    parser.add_argument('--camera', default=0, type=int)
+    parser.add_argument('--camAPI', default=0, type=int)#opencv videocapture api
+    parser.add_argument('--frameW', default=640, type=int)#opencv frame width
+    parser.add_argument('--frameH', default=480, type=int)#opencv frame height
     args = parser.parse_args()
 
     vid = cv2.VideoCapture(args.camera,args.camAPI)
